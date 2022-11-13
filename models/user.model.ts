fe977e7c03201444f4b0ecab40dd467bd797f6cc
AppDeveloperVM <vicmr1994@gmail.com>
2022-11-13 17:38:11 +0100

    add models - Profile, User, Repo

---
+++ models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    login: string;
+    id: number;
+    name: string;
+    email: string;
+    location: string;
+    avatar_url: string;
+    public_repos: number
+    public_gists: number;
+}
